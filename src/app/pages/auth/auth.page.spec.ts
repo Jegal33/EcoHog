bc36087057e26c26072946ae2477bc7ce4b48243
ChubiLanes <134746792+Chubi12@users.noreply.github.com>
2023-11-22 15:19:46 -0300

    testing
    pruebas unitarias para login y registrarse esto no afecta a la aplicacion ya que son pruebas separas de la app

--- src/app/pages/auth/auth.page.spec.ts
+++ src/app/pages/auth/auth.page.spec.ts
@@ -1,17 +1,84 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
+import { ReactiveFormsModule } from '@angular/forms';
+import { IonicModule } from '@ionic/angular';
+import { FirebaseService } from 'src/app/services/firebase.service';
+import { UtilsService } from 'src/app/services/utils.service';
 import { AuthPage } from './auth.page';
+
 
 describe('AuthPage', () => {
   let component: AuthPage;
   let fixture: ComponentFixture<AuthPage>;
+  let firebaseServiceMock: jasmine.SpyObj<FirebaseService>;
+  let utilsServiceMock: jasmine.SpyObj<UtilsService>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
+    const firebaseSpy = jasmine.createSpyObj('FirebaseService', ['signIn', 'getUserInfo']);
+    const utilsSpy = jasmine.createSpyObj('UtilsService', ['loading', 'presentToast']);
+
+    TestBed.configureTestingModule({
+      declarations: [AuthPage],
+      imports: [ReactiveFormsModule, IonicModule.forRoot()],
+      providers: [
+        { provide: FirebaseService, useValue: firebaseSpy },
+        { provide: UtilsService, useValue: utilsSpy }
+      ]
+    }).compileComponents();
+
     fixture = TestBed.createComponent(AuthPage);
     component = fixture.componentInstance;
-    fixture.detectChanges();
+    firebaseServiceMock = TestBed.inject(FirebaseService) as jasmine.SpyObj<FirebaseService>;
+    utilsServiceMock = TestBed.inject(UtilsService) as jasmine.SpyObj<UtilsService>;
   }));
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should call signIn method and getUserInfo method on submit', async () => {
+    const mockUser = { email: 'test@example.com', password: 'password' };
+    const mockUserInfo = { uid: 'testUid', name: 'Test User' };
+
+    firebaseServiceMock.signIn.and.returnValue(Promise.resolve({ user: { uid: 'testUid' } }));
+    firebaseServiceMock.getUserInfo.and.returnValue(Promise.resolve(mockUserInfo));
+    utilsServiceMock.loading.and.returnValue(Promise.resolve({ present: () => {}, dismiss: () => {} }));
+
+    component.form.setValue(mockUser);
+    await component.submit();
+
+    expect(firebaseServiceMock.signIn).toHaveBeenCalledWith(mockUser);
+    expect(firebaseServiceMock.getUserInfo).toHaveBeenCalledWith('testUid');
+    expect(utilsServiceMock.loading).toHaveBeenCalled();
+    expect(utilsServiceMock.presentToast).not.toHaveBeenCalled();
+  });
+
+  it('should show error toast when signIn fails', async () => {
+    const mockUser = { email: 'test@example.com', password: 'password' };
+
+    firebaseServiceMock.signIn.and.returnValue(Promise.reject('Sign in failed'));
+    utilsServiceMock.loading.and.returnValue(Promise.resolve({ present: () => {}, dismiss: () => {} }));
+
+    component.form.setValue(mockUser);
+    await component.submit();
+
+    expect(firebaseServiceMock.signIn).toHaveBeenCalledWith(mockUser);
+    expect(utilsServiceMock.loading).toHaveBeenCalled();
+    expect(utilsServiceMock.presentToast).toHaveBeenCalledWith('Sign in failed', 'danger');
+  });
+
+  it('should show error toast when getUserInfo fails', async () => {
+    const mockUser = { email: 'test@example.com', password: 'password' };
+
+    firebaseServiceMock.signIn.and.returnValue(Promise.resolve({ user: { uid: 'testUid' } }));
+    firebaseServiceMock.getUserInfo.and.returnValue(Promise.reject('Get user info failed'));
+    utilsServiceMock.loading.and.returnValue(Promise.resolve({ present: () => {}, dismiss: () => {} }));
+
+    component.form.setValue(mockUser);
+    await component.submit();
+
+    expect(firebaseServiceMock.signIn).toHaveBeenCalledWith(mockUser);
+    expect(firebaseServiceMock.getUserInfo).toHaveBeenCalledWith('testUid');
+    expect(utilsServiceMock.loading).toHaveBeenCalled();
+    expect(utilsServiceMock.presentToast).toHaveBeenCalledWith('Get user info failed', 'danger');
   });
 });
--- src/app/pages/auth/sign-up/sign-up.page.spec.ts
+++ src/app/pages/auth/sign-up/sign-up.page.spec.ts
@@ -1,17 +1,58 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
+import { ReactiveFormsModule } from '@angular/forms';
+import { IonicModule } from '@ionic/angular';
 import { SignUpPage } from './sign-up.page';
+import { FirebaseService } from 'src/app/services/firebase.service';
+import { UtilsService } from 'src/app/services/utils.service';
+import { of } from 'rxjs';
 
 describe('SignUpPage', () => {
   let component: SignUpPage;
   let fixture: ComponentFixture<SignUpPage>;
+  let firebaseService: FirebaseService;
+  let utilsService: UtilsService;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
+    TestBed.configureTestingModule({
+      declarations: [SignUpPage],
+      imports: [ReactiveFormsModule, IonicModule],
+      providers: [
+        FirebaseService,
+        UtilsService,
+      ],
+    }).compileComponents();
+  }));
+
+  beforeEach(() => {
     fixture = TestBed.createComponent(SignUpPage);
     component = fixture.componentInstance;
+    firebaseService = TestBed.inject(FirebaseService);
+    utilsService = TestBed.inject(UtilsService);
     fixture.detectChanges();
-  }));
+  });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should call signUp method', () => {
+    const signUpSpy = spyOn(firebaseService, 'signUp').and.returnValue(of({ user: { uid: '123' } }));
+
+    // Simulate user input and trigger signUp method
+
+    expect(signUpSpy).toHaveBeenCalled();
+    expect(component.errorMessage).toBeNull();
+    expect(component.successMessage).toBe('User signed up successfully');
+  });
+
+  it('should display error message on signUp failure', () => {
+    const signUpSpy = spyOn(firebaseService, 'signUp').and.returnValue(of({ error: 'Invalid email' }));
+
+    // Simulate user input and trigger signUp method
+
+    expect(signUpSpy).toHaveBeenCalled();
+    expect(component.errorMessage).toBe('Invalid email');
+    expect(component.successMessage).toBeNull();
+  });
+
 });
